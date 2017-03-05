44f925a7118b6af07c9cc5ed616e57c559f378b2
Nicholas Omer Chiasson <nicholaschiasson@namnamnamnam.com>
2017-03-04 20:33:26 -0500

    Adding build script and travis build file

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+rm -rf bin
+cp -rf src bin
+python -m compileall bin
+python -OO -m compileall bin
+find bin -name *.py | while read f; do rm $f; done
+tar cvzf bin/nai.tgz -C bin nai
